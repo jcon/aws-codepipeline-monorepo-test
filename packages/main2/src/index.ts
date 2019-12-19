3fe47bf709765fc870f1aa290c8949de3bdd8abf
Jim Connell <jim@meetup.com>
2019-12-19 00:57:18 -0500

    update comments for main2.

--- packages/main2/src/index.ts
+++ packages/main2/src/index.ts
@@ -1,6 +1,7 @@
 import { myFoo } from '@meetup/foo';
 import { myBar } from '@meetup/bar';
 
+// Added a comment; This shouldn't trigger a build.
 const main = () => {
     console.log(`MAIN 2\n\nfoo: ${myFoo()}\nbar: ${myBar()}`);
 }
