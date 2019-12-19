5312b86dd39cc41cc93425a6d2870ee6aae3ed8a
Jim Connell <jim@meetup.com>
2019-12-19 00:40:10 -0500

    update main 1.

--- packages/main1/src/index.ts
+++ packages/main1/src/index.ts
@@ -2,7 +2,7 @@ import { myFoo } from '@meetup/foo';
 import { myBar } from '@meetup/bar';
 
 const main = () => {
-    console.log(`MAIN 2\nfoo: ${myFoo()}\nbar: ${myBar()}`);
+    console.log(`MAIN 1\nfoo: ${myFoo()}\nbar: ${myBar()}`);
 }
 
 main();
