d4441e32382f709f67d2600e68870c9c56d324d3
Jim Connell <jim@meetup.com>
2019-12-19 00:50:23 -0500

    added a comment to main2.

--- packages/main1/src/index.ts
+++ packages/main1/src/index.ts
@@ -1,6 +1,7 @@
 import { myFoo } from '@meetup/foo';
 import { myBar } from '@meetup/bar';
 
+// This program prints things to the console.
 const main = () => {
     console.log(`MAIN 1\nfoo: ${myFoo()}\nbar: ${myBar()}`);
 }
