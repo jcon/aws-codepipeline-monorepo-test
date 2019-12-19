69c0ccdcc2fd15d47be3dd6abb6b104ba01073d8
Jim Connell <jim@meetup.com>
2019-12-18 23:35:59 -0500

    fix program name for main2. should not trigger build

--- packages/main2/src/index.ts
+++ packages/main2/src/index.ts
@@ -2,7 +2,7 @@ import { myFoo } from '@meetup/foo';
 import { myBar } from '@meetup/bar';
 
 const main = () => {
-    console.log(`MAIN 1\n\nfoo: ${myFoo()}\nbar: ${myBar()}`);
+    console.log(`MAIN 2\n\nfoo: ${myFoo()}\nbar: ${myBar()}`);
 }
 
 main();
