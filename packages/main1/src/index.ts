07c098f168ff3546fc0e8ee70f7f9e291d563677
Jim Connell <jim@meetup.com>
2019-12-19 01:03:49 -0500

    trigger another main1 deploy

--- packages/main1/src/index.ts
+++ packages/main1/src/index.ts
@@ -2,7 +2,7 @@ import { myFoo } from '@meetup/foo';
 import { myBar } from '@meetup/bar';
 
 // This program prints things to the console.
-// This update should trigger a build (again).
+// This update should trigger a build (again again).
 const main = () => {
     console.log(`MAIN 1\nfoo: ${myFoo()}\nbar: ${myBar()}`);
 }
